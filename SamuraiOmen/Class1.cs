71d45ddba2e7a1d47b700f4b2e360a7699a99858
EXtermin <Damian.meijer@gmail.com>
2019-05-20 19:51:09 +0200

    Initial Upload
    Added basic solution

---
+++ SamuraiOmen/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiOmen
+{
+    public class Class1
+    {
+
+    }
+}
